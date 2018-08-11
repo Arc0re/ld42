28998330c4a6009c062bd994166b77f41e9b57fc
Arc0re <paperboycheero@yahoo.com>
2018-08-11 11:30:48 +0200

    Stars! 2D grid as space map.

--- game.js
+++ game.js
@@ -2,14 +2,91 @@ const FPS_DIVIDER = 1000.0,
   SPRITESHEET_PATH = "gfx/spritesheet.png",
   BLOCK_WIDTH = 8,
   BLOCK_HEIGHT = 8,
-  CANVAS_SCALE = 4;
+  CANVAS_SCALE = 4,
+  SPACE_WIDTH = 200,
+  SPACE_HEIGHT = 200,
+  SPACETILE_STAR0 = 1,
+  SPACETILE_STAR1 = 2,
+  SPACETILE_STAR2 = 3;
 
-var canvas = document.getElementById("game_canvas");
-ctx = canvas.getContext("2d"),
+var canvas = document.getElementById("game_canvas"),
+  ctx = canvas.getContext("2d"),
   lastTime = null,
   spriteSheet = new Image(),
   doneLoading = false,
   windowWidth = 0, windowHeight = 0;
+
+// Type definitions
+
+class Sprite {
+  constructor(srcX, srcY, width, height, name) {
+    this.srcX = srcX;
+    this.srcY = srcY;
+    this.width = width;
+    this.height = height;
+    this.name = name;
+    this.x = 0;
+    this.y = 0;
+  }
+
+  setPos(vec) {
+    this.x = vec.x;
+    this.y = vec.y;
+  }
+
+  getPos() {
+    return {x: this.x, y: this.y};
+  }
+
+  render() {
+    ctx.drawImage(spriteSheet, this.srcX, this.srcY, this.width, this.height, this.x, this.y, this.width, this.height);
+  }
+}
+
+class Space {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.map = [];
+  }
+
+  init() {
+    for (var x = 0; x < this.width; x++) {
+      for (var y = 0; y < this.height; y++) {
+        var tileNum = randInt(1, 4);
+        var newSprite = null;
+        switch (tileNum) {
+          case SPACETILE_STAR0:
+            newSprite = new Sprite(8, 13, BLOCK_WIDTH, BLOCK_HEIGHT, "star_block_0");
+            break;
+          case SPACETILE_STAR1:
+            newSprite = new Sprite(17, 13, BLOCK_WIDTH, BLOCK_HEIGHT, "star_block_1");
+            break;
+          default:
+          case SPACETILE_STAR2:
+            newSprite = new Sprite(26, 13, BLOCK_WIDTH, BLOCK_HEIGHT, "star_block_2");
+            break;
+        }
+        this.map[x + y * this.width] = newSprite;
+      }
+    }
+  }
+
+  render() {
+    for (var x = 0; x < this.width; x++) {
+      for (var y = 0; y < this.height; y++) {
+        var currentTile = this.map[x + y * this.width];
+        if (currentTile) {
+          currentTile.setPos({x: x * currentTile.width, y: y * currentTile.height});
+          currentTile.render();
+        }
+      }
+    }
+  }
+}
+
+var sprite = new Sprite(0, 0, BLOCK_WIDTH, BLOCK_HEIGHT);
+var space = new Space(SPACE_WIDTH, SPACE_HEIGHT);
 
 // Entry point
 window.addEventListener('load', function () {
@@ -44,6 +121,10 @@ function init() {
 
   ctx.scale(CANVAS_SCALE, CANVAS_SCALE);
 
+  // Initialisation
+  space.init();
+  console.log(space);
+
   // Loading
   spriteSheet.src = SPRITESHEET_PATH;
 
@@ -63,11 +144,17 @@ function tick() {
 }
 
 function update(delta) {
-  // console.log("tick: " + delta);
+  sprite.setPos({x: 20, y: 20});
 }
 
 function render() {
   if (doneLoading) {
-    ctx.drawImage(spriteSheet, 0, 0, BLOCK_WIDTH, BLOCK_HEIGHT, 10, 10, BLOCK_WIDTH, BLOCK_HEIGHT);
+    space.render();
+    sprite.render();
   }
+}
+
+// Utils
+function randInt(from, to) {
+  return Math.floor(Math.random() * (to - from + 1) + from);
 }
